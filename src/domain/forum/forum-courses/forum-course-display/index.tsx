0d69aaf3fccf69cb50f66f49448ebd3979c89513
Leonardo Vianna <leonardo.vianna.f@gmail.com>
2019-05-18 18:38:49 -0300

    Alt

--- src/domain/forum/forum-courses/forum-course-display/index.tsx
+++ src/domain/forum/forum-courses/forum-course-display/index.tsx
@@ -14,7 +14,7 @@ export default function ForumCourseDisplay({ course }: ForumCourseDisplayProps) 
     <div className="container forum-course-display">
       <div className="row align-items-center">
         <div className="col-2 overflow-hidden course-icon px-0">
-          <img className="pr-2" src="/assets/img/fire.jpg" />
+          <img className="pr-2" src="/assets/img/fire.jpg" alt="Course"/>
         </div>
         <div className="col-10">
           <Button id="course" fullWidth>
